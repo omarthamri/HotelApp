5fccc3724c19d63bb2eb198b497d5990168acdc6
Omar Thamri <omarthamri4@gmail.com>
2019-07-06 17:38:40 +0100

    Initial Commit

---
+++ HotelApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HotelApp
+//
+//  Created by Omar Thamri on 06/07/2019.
+//  Copyright © 2019 MACBOOK PRO RETINA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
